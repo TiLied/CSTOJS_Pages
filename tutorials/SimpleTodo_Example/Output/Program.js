e7ce2bcb6d68921f1bdec245ae4d08f278a35b03
TiLied <TiLied@users.noreply.github.com>
2025-12-11 23:49:26 +0300

    Added simple todo example.

---
+++ tutorials/SimpleTodo_Example/Output/Program.js
@@ -0,0 +1,58 @@
+
+globalThis.window.document.addEventListener("DOMContentLoaded", (e) => {
+	let body = globalThis.window.document.body;
+
+	let todoForm = body.querySelector("#TodoForm");
+	let ulList = body.querySelector("#List");
+
+	
+	todoForm.addEventListener("submit", (e) => {
+		
+				
+		e.preventDefault();
+
+		let data = new FormData(todoForm);
+		
+				
+		AddEntry(data, ulList);
+		
+		console.log(data);
+	}, true);
+}, true);
+
+function AddEntry(data, ulList) {
+	let divElement = globalThis.window.document.createElement("div");
+
+	//Hack, "Union237" is not converting to a string implicitly.
+	//I need another way around "Union237" to string, not string to "Union237"
+	//Union237 test = "str";
+	let todo_str = data.get("todo") + " ";
+
+	let todoText = globalThis.window.document.createTextNode(todo_str);
+
+	let buttonElement = globalThis.window.document.createElement("button");
+	
+		
+	buttonElement.textContent = "Delete";
+	
+	let liElement = globalThis.window.document.createElement("li");
+
+	
+	divElement.appendChild(todoText);
+	
+		
+	divElement.appendChild(buttonElement);
+
+	
+	liElement.appendChild(divElement);
+
+	
+	ulList.appendChild(liElement);
+	
+		
+	buttonElement.addEventListener("click", ()=> { DeleteEntry(liElement); }, true);
+}
+
+function DeleteEntry(li) {
+	li.remove();
+}
---
+++ tutorials/simple-todo.md
@@ -0,0 +1,19 @@
+## Simple todo
+
+> [!NOTE]
+> cstojs-cli 0.1.2 is used.
+
+cstojs_options.xml:
+[!code-xml[](./SimpleTodo_Example/cstojs_options.xml)]
+
+Program.cs:
+[!code-csharp[](./SimpleTodo_Example/Program.cs)]
+
+Translated js:
+[!code-javascript[](./SimpleTodo_Example/Output/Program.js)]
+
+See the full source code: https://github.com/TiLied/CSTOJS_Pages/tree/main/tutorials/SimpleTodo_Example
+
+Live example:
+
+<iframe src="./SimpleTodo_Example/Output/index.html"></iframe>
